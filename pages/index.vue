aca5533558febdaf647d96c61e018b7b7aa431a2
celineworks <output.ed11@gmail.com>
2022-01-21 21:38:33 +0900

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -243,14 +243,7 @@ export default {
     },
 
     mounted(){
-        if (navigator.userAgent.indexOf('iPhone') > 0 || navigator.userAgent.indexOf('Android') > 0 && navigator.userAgent.indexOf('Mobile') > 0) {
-            // sm
-        } else if (navigator.userAgent.indexOf('iPad') > 0 || navigator.userAgent.indexOf('Android') > 0) {
-            // tab
-        } else {
-            // PC
         luxy.init();
-        }
     }
 }
 
